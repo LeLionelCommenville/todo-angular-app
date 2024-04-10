f25a94154ea69a5fc71cefb9288d97052c752868
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-04-10 13:44:53 +0200

    feat: add a todo list component for showing the list of todo

---
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TodoListComponent } from './todo-list.component';
+import { CommonModule } from '@angular/common';
+
+describe('TodoListComponent', () => {
+  let component: TodoListComponent;
+  let fixture: ComponentFixture<TodoListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        TodoListComponent,
+        CommonModule
+
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TodoListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { TodoService } from '../todo.service';
+import { Todo } from '../todo';
+import { TodoComponent } from '../todo/todo.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [
+    TodoComponent,
+    CommonModule
+
+  ],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
+})
+export class TodoListComponent {
+  title: string = 'Todo List';
+  todoService : TodoService = inject(TodoService);
+  todoList : Todo[] = [];
+
+  constructor() {
+    this.todoList = this.todoService.getAllTodos();
+  }
+}
