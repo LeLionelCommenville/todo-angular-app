048976b934febeeb321f7440c0c1d787e1f55e63
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-04-27 19:57:52 +0200

    feat: puting edit todo into a modal

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Todo } from '../todo';
 import { EditTodoComponent } from '../edit-todo/edit-todo.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [EditTodoComponent],
+  imports: [EditTodoComponent, CommonModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
   @Input() todo!: Todo;
+  showEditModal = false;
+
+  toggleModal(){
+    this.showEditModal = !this.showEditModal;
+  }
 }
