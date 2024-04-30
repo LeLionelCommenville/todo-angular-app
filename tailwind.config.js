0fcbfaae78b04fe9578234fc94daf16b84040e46
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-04-30 19:32:15 +0200

    style: change the background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background-blue': '#112494',
+      },
+    },
   },
   plugins: [],
 }
